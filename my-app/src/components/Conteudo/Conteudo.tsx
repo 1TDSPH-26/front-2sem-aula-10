757c1fd78c0b6300f0f90b22af5251b942b1e0af
Zayon101 <zayon.alves.marques@gmail.com>
2026-09-02 09:30:59 -0300

    feat: Exemplo de apresentação de imagem local.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,3 +1,5 @@
+import berrete from "../../img/berrete.png";
+
 export default function Conteudo() {
   return (
     <main>
@@ -9,17 +11,20 @@ export default function Conteudo() {
 
       <section>
         <h2>Lista aleatória</h2>
-
         <p>Produto aleatório - R$ 14,90</p>
-
 
     <figure>
         <img
             src="https://placehold.co/600x400/FFFFF0/FFFFFF/png"
             alt="Imagem placeholder"/>
-
-        <figcaption>Imagem de exemplo 600x400</figcaption>
+            <figcaption>Imagem de exemplo 600x400</figcaption>
         </figure>
+      
+     <figure>
+         <img src={berrete} alt="Berrete" />
+         <figcaption>Killua com Berrete do Lord Renan</figcaption>
+        </figure>
+
       </section>
     </main>
 );
