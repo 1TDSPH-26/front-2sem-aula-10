a997e3215035c3651b783c43f70e88a7d10e5def
giovannidevazevedo <giovanniazevedo065@gmail.com>
2026-09-02 09:04:49 -0300

    feat: primeiros componentes criados

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import Cabecalho from "./components/cabecalho/Cabecalho";
+import Conteudo from "./components/conteudo/Conteudo";
+import Rodape from "./components/rodape/Rodape";
+
+export default function App(){
+  return (
+    <div>
+      <Cabecalho />
+      <Conteudo />
+      <Rodape/>
+    </div>
+  )
+}
---
+++ my-app/src/components/conteudo/Conteudo.tsx
@@ -0,0 +1,8 @@
+export default function Conteudo() {
+    return (
+        <main>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores sit, nobis ab exercitationem, et minus velit incidunt voluptatem quia, sequi corporis! Dolores omnis, error dolor eligendi commodi accusamus alias suscipit!
+        </main>
+
+    )
+}
