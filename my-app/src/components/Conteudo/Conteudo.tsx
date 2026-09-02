972cf8b273bfe76b1beaf43b426eaa3ef34952a8
Gizetto61 <gigiozetto@gmail.com>
2026-09-02 09:19:07 -0300

    feat: Exemplo de apresentação de imagem.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -5,6 +5,13 @@ export default function Conteudo(){
                 <h2>Conteúdo Básico</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id doloremque sint esse odio tempore ipsam, quas debitis corrupti cupiditate necessitatibus unde ratione nam? Quos optio eos rem qui placeat.</p>
             </section>
+            <section>
+                <h2>Exemplo de Imagens</h2>
+                <figure>
+                    <img src="https://placehold.co/400x400/c1c1c1/000000/png" alt="Imagem Exemplo1" />
+                    <figcaption>Bloco de Imagem 400 x 400</figcaption>
+                </figure>
+            </section>
         </main>
     );
 }
