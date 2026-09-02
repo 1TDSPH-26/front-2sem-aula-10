725eaf99ff2c8198ead04ca18d36449df823b379
Sammer-07 <vertigosam1992@gmail.com>
2026-09-02 08:21:53 -0300

    Deletando e alterando arquivos do repositorio

---
+++ y/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Site</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nostrum vero labore! Sed accusantium quasi magni praesentium reiciendis doloribus omnis veritatis eos quidem magnam minus tempora fugit, iure enim odit.</p>
+    </main>
+  );
+}
+
+export default App;
