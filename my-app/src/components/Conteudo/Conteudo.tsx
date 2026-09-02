c7975db34e6c18bccde1f3de07f5564e4aa0ac89
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-02 09:28:41 -0300

    feat: Referências de imagens
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,18 +1,40 @@
+import imgQuadrada from "../../img/quadrado.png";
 
 export default function Conteudo() {
   return (
     <main>
-        <section>
-            <h2>Conteúdo Básico</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos soluta sed vero reiciendis enim nam, aut perferendis, sit velit ratione placeat, distinctio eius rerum maxime id perspiciatis repudiandae non! Tenetur.</p>
-        </section>
-        <section>
-            <h2>Exemplo de Imagens</h2>
-            <figure>
-                <img src="https://placehold.co/400x400/c1c1c1/000000/png" alt="Exemplo de imagem" />
-                <figcaption>Bloco de Imagem 400 x 400</figcaption>
-            </figure>
-        </section>
+      <section>
+        <h2>Conteúdo Básico</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          soluta sed vero reiciendis enim nam, aut perferendis, sit velit
+          ratione placeat, distinctio eius rerum maxime id perspiciatis
+          repudiandae non! Tenetur.
+        </p>
+      </section>
+      <section>
+        <h2>Exemplo de Imagens</h2>
+        {/* Esta imagem tem referência externa */}
+        <figure>
+          <img
+            src="https://placehold.co/400x400/c1c1c1/000000/png"
+            alt="Exemplo de imagem"
+          />
+          <figcaption>Bloco de Imagem 400 x 400</figcaption>
+        </figure>
+
+        {/* Esta imagem tem referência interna na /src */}
+        <figure>
+          <img src={imgQuadrada} alt="Exemplo de imagem" />
+          <figcaption>Bloco de Imagem 400 x 400</figcaption>
+        </figure>
+
+        {/* Esta imagem tem referência interna na public */}
+        <figure>
+          <img src="/img/quadrado.png" alt="Exemplo de imagem" />
+          <figcaption>Bloco de Imagem 400 x 400</figcaption>
+        </figure>
+      </section>
     </main>
-  )
+  );
 }
