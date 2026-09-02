c94b0b6cfe877521f5489e11aaca6f76843ca8d5
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-02 09:54:14 -0300

    feat : Iniciando o useState esconde seção.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import imgQuadrada from "../../img/quadrado.png";
 
 export default function Conteudo() {
+
+      const[mostraSecao, setMostraSecao] = useState(false);
+
+      const alternaSecao = () =>{
+
+        setMostraSecao(!mostraSecao);
+        console.log(mostraSecao);
+      }
+
+
   return (
     <main>
       <section>
@@ -35,6 +46,12 @@ export default function Conteudo() {
           <figcaption>Bloco de Imagem 400 x 400</figcaption>
         </figure>
       </section>
+
+      <section >
+        <h2>Especial</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, vitae recusandae? Inventore impedit earum nisi reiciendis provident autem cum aliquam natus excepturi ut distinctio, laborum veritatis, veniam fugiat exercitationem quam iusto accusantium culpa reprehenderit quas quibusdam. Nulla, quibusdam cumque? Quibusdam dolorum dolore deserunt. Assumenda eos voluptatibus dolores eaque fugit quod?</p>
+      </section>
+      <button onClick={alternaSecao}>{mostraSecao ? "ESCONDER" : "MOSTRAR"}</button>
     </main>
   );
 }
