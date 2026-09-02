1dae5a94d8b5662ceda300f6a7d4c161286e59e8
Diego <diegotrugillo.14@gmail.com>
2026-09-02 09:09:39 -0300

    feat: movendo para as pastas

--- my-app/src/components/Conteudo.tsx
+++
@@ -1,9 +0,0 @@
-function Conteudo() {
-  return (
-    <main>
-      <p>Este é o conteúdo principal da aplicação.</p>
-    </main>
-  )
-}
-
-export default Conteudo
---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,10 @@
+function Conteudo() {
+  return (
+    <main>
+      <p>Este é o conteúdo principal da aplicação.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos sapiente, quasi dolores unde possimus sequi odit obcaecati at? Tempora ullam laudantium mollitia, illum necessitatibus sed ab quis doloremque veniam nam.</p>
+    </main>
+  )
+}
+
+export default Conteudo
