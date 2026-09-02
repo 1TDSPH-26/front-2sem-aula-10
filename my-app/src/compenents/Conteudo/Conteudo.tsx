b09df069b6154384c3ee4889d1b1bf9fc360f2ba
Enzo Nukui <enzonukui2007@gmail.com>
2026-09-02 09:19:17 -0300

    feat: exemplo de apreentação de imagem

--- my-app/src/compenents/Conteudo/Conteudo.tsx
+++ my-app/src/compenents/Conteudo/Conteudo.tsx
@@ -6,6 +6,11 @@ export default function Conteudo(){
                 <h2>Conteudo basico</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In deserunt error, fugiat dolor laboriosam aut animi consectetur corrupti quae explicabo pariatur fuga libero sed ipsum? Optio dolorum quae a soluta?</p>
             </section>
+            <section>
+                <h2>Exemplo de imagens</h2>
+            </section>
+            <img src="https://placehold.co/400x400/c1c1c1/000000/png" alt="Exemplo de imagem" />
+            <figcaption>Bloco de imagem 400 x 400 </figcaption>
         </main>
     )
 
