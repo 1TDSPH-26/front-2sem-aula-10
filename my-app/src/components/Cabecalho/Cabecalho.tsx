235d1c611a85c73470a1024e75b3728d4cabcb2f
Zayon101 <zayon.alves.marques@gmail.com>
2026-09-02 09:04:45 -0300

    feat: Criação dos componentes Cabecalho, Conteudo e Rodape e modularização no App.tsx

---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,13 @@
+export default function Cabecalho() {
+  return (
+    <header>
+      <h1>Minha Página React</h1>
+
+      <nav>
+        <a href="#">Início</a>
+        <a href="#">Produtos</a>
+        <a href="#">Contato</a>
+      </nav>
+    </header>
+  );
+}
---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,19 @@
+export default function Conteudo() {
+  return (
+    <main>
+      <h2>Conteúdo</h2>
+
+      <p>
+        Este é o conteúdo principal da minha aplicação React.
+      </p>
+
+      <section>
+        <h2>Produtos</h2>
+
+        <p>Produto 1 - R$ 100,00</p>
+        <p>Produto 2 - R$ 50,00</p>
+        <p>Produto 3 - R$ 10,00</p>
+      </section>
+    </main>
+  );
+}
