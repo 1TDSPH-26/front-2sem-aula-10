8282616d3cb8a0e9b168c34be90b1046ba9b90d1
Enzo Nukui <enzonukui2007@gmail.com>
2026-09-02 09:57:34 -0300

    feat: adiciona botão de mostrar seção

--- my-app/src/compenents/Conteudo/Conteudo.tsx
+++ my-app/src/compenents/Conteudo/Conteudo.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
 import imgQuadrada from "../../img/quadrado.png"
 
-export default function Conteudo(){
+export default function Conteudo() {
 
-    return(
+        const[mostraSecao, setMostraSecao] = useState(false);
+ 
+      const alternaSecao = () =>{
+ 
+        setMostraSecao(!mostraSecao);
+        console.log(mostraSecao);
+      }
+
+    return (
         <main>
             <section>
                 <h2>Conteudo basico</h2>
@@ -11,8 +20,8 @@ export default function Conteudo(){
             <section>
                 <h2>Exemplo de imagens</h2>
                {/*Esta imagem tem referencia externa*/} 
-            </section>
-            <img src="https://placehold.co/400x400/c1c1c1/000000/png" alt="Exemplo de imagem" />
+
+               <img src="https://placehold.co/400x400/c1c1c1/000000/png" alt="Exemplo de imagem" />
             <figcaption>Bloco de imagem 400 x 400 </figcaption>
 
             {/*Esta imagem tem referencia interna*/} 
@@ -24,7 +33,16 @@ export default function Conteudo(){
 
             <img src="/img/quadrado.png" alt="Exemplo de imagem" />
             <figcaption>Bloco de imagem 400 x 400 </figcaption>
+            </section>
+
+            <section>
+                <h2>Especial</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quae sapiente labore recusandae ab magni laborum iure, veniam excepturi delectus sunt omnis sint culpa pariatur illum asperiores ut aliquid aut sequi ratione soluta accusamus. At, vitae officiis hic adipisci aliquam inventore laboriosam dignissimos ut placeat, magni odit temporibus consequatur harum.</p>
+            </section>
+            <button onClick={alternaSecao}>{mostraSecao ? "ESCONDER" : "MOSTRAR"}</button>
+            
         </main>
     )
+    
 
 }
