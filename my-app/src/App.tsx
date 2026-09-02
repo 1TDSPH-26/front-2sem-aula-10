6071934da54d7a38aa5237b0dc2a193fec908d14
henriquebonachela <carabantecarvalho@gmail.com>
2026-09-02 09:39:03 -0300

    feat: Criando componentes funcionais de modularização com Cabecalho, Conteudo e Rodape.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,13 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Rodape/Rodape";
+
 export default function App(){
   return (
-    <div>App</div>
+    <div>
+      <Cabecalho/>
+      <Conteudo/>
+      <Rodape/>
+    </div>
   )
 }
