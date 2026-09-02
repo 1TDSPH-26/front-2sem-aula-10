aeb12edb5f91ddf01016cf89da3b1b44596f1ad4
Gizetto61 <gigiozetto@gmail.com>
2026-09-02 09:09:26 -0300

    feat: Criando componentes funcionáis de modularização. Cabeçalho, Conteúdo e Rodapé.

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,10 @@
+export default function Conteudo(){
+    return(
+        <main>
+            <section>
+                <h2>Conteúdo Básico</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id doloremque sint esse odio tempore ipsam, quas debitis corrupti cupiditate necessitatibus unde ratione nam? Quos optio eos rem qui placeat.</p>
+            </section>
+        </main>
+    );
+}
