c28bba7e80cd7a032cd4cdf606ce7ddd4b772817
Giovanni <giovanni.zavam2008@gmail.com>
2026-09-02 08:25:14 -0300

    feat: Llimpeza do boiler plate

---
+++ my-app/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App(){
+
+    return(
+        <div>
+            <h1>Bem-vindo à minha aplicação</h1>
+            <p>Este e meu exemplo teste da minha app.tsx</p>
+        </div>
+    );
+}
