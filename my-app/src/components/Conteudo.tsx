f2b1ddbfdcd97e459140f86bf6c53eeed8219638
EuThiaguera <piedadethiago7@gmail.com>
2026-09-02 09:55:55 -0300

    feat: Criação de mudamça de sessão

--- my-app/src/components/Conteudo.tsx
+++ my-app/src/components/Conteudo.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import imgQuadrada from "../img/quadrado.png";
 
 export default function Conteudo() {
+    const[mostraSecao, setMostraSecao] = useState(false);
+
+   const alternaSecao = () => {
+  setMostraSecao(!mostraSecao);
+  console.log(mostraSecao);
+}
+
   return (
     <main>
         <section>
@@ -15,15 +23,23 @@ export default function Conteudo() {
             alt="Exemplo de imagem" />
             <figcaption>Bloco de imagem 600 x 400</figcaption>
             </figure>
+            {/*Esta imagem tem referência interna na /src*/}
             <figure>
             <img src={imgQuadrada} alt="Exemplo de imagem" />
             <figcaption>Bloco de imagem 600 x 400</figcaption>
             </figure>
+              {/*Esta imagem tem referência interna na /public*/}
             <figure>
             <img src="/quadrado.png" alt="Exemplo de imagem" />
             <figcaption>Bloco de imagem 600 x 400</figcaption>
             </figure>
         </section>
+
+        <section>
+            <h2>Especial</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam dolor, animi quisquam quidem illo eaque. Ullam expedita ad dolorum animi porro. Mollitia omnis necessitatibus hic est incidunt aliquid placeat delectus dolores numquam corporis eveniet soluta qui corrupti quidem, dolorum ab aut, veniam tempora. Laborum suscipit sequi eius sit porro soluta?</p>
+        </section>
+        <button onClick={alternaSecao}>{mostraSecao ? "ESCONDER" : "MOSTRAR"}</button>
     </main>
   )
 }
