8461f5e90bf98824925f441ecd00eb5d8eb22b72
marifernandesgm <marifernandesgm17@gmail.com>
2026-09-02 09:18:26 -0300

    feat: Exemplo de apresentação de imagem.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -8,6 +8,16 @@ export default function Conteudo(){
                 </h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, illo expedita provident, repudiandae pariatur, perferendis laborum exercitationem itaque officiis molestias vitae! Sapiente aliquid ea vitae corrupti reprehenderit quo saepe distinctio!</p>
             </section>
+            <section>
+                <h2>
+                    Exemplo de Imagens
+                </h2>
+                <figure>
+                    <img src="https://placehold.co/400x400/c1c1c1/000000/png"
+                    alt="Exemplo de Imagem"></img>
+                    <figcaption>Bloco de Imagem 400 x 400</figcaption>
+                </figure>
+            </section>
         </main>
     
     );
