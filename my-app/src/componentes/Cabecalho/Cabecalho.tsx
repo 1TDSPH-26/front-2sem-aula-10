62ef66728e6e346f8d023263cc0e533852eff530
gabefeitosa <gabrielfeitosa321@outlook.com>
2026-09-02 09:19:24 -0300

    feat: exemplo de imagem aplicado

--- my-app/src/componentes/Cabecalho/Cabecalho.tsx
+++ my-app/src/componentes/Cabecalho/Cabecalho.tsx
@@ -6,6 +6,14 @@ export default function Cabecalho(){
                 <h1>
                     Cabeçalho
                 </h1>
+                <nav>
+                    <ul>
+                        <li>Item-1</li>
+                        <li>Item-2</li>
+                        <li>Item-3</li>
+
+                    </ul>
+                </nav>
             </header>
         </div>
     );
--- my-app/src/componentes/Conteudo/Conteudo.tsx
+++ my-app/src/componentes/Conteudo/Conteudo.tsx
@@ -2,12 +2,23 @@ export default function Conteudo(){
 
     return(
         <main>
-            <h2>Conteudo</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                Corrupti beatae cumque blanditiis voluptate voluptatum 
-                doloribus tenetur molestias dicta enim! Odio, sit? Delectus 
-                quo ex harum minus quam quasi illum repellat?
-            </p>
+            <section>
+                <h2>Conteudo Básico</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Corrupti beatae cumque blanditiis voluptate voluptatum 
+                    doloribus tenetur molestias dicta enim! Odio, sit? Delectus 
+                    quo ex harum minus quam quasi illum repellat?
+                </p>
+            </section>
+            <section>
+                <h2>Exemplo de imagens</h2>
+                <figure>
+                    <img src="https://placehold.co/600x400/1c1c1c1/FFFFFF/png" alt="" />
+                </figure>
+                <figcaption>Bloco de imagem 600 x 400</figcaption>
+            </section>
+            
         </main>
     );
 }
