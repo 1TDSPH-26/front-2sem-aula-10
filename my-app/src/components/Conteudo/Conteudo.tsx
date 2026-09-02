61c27dd30471a42e29beceb8e9d990298b060df3
Nicolas Sousa <niolaspaixao34@gmail.com>
2026-09-02 09:54:34 -0300

    feat: Iniciando o useState esconde seção.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import imgQuadrado from "../../img/png.png";
 
 
 
 export default function Conteudo() {
+
+
+      const[mostraSecao, setMostraSecao] = useState(false);
+
+      const alternaSecao = () => {
+      
+        setMostraSecao(!mostraSecao);
+        console.log(mostraSecao);
+
+      }
+
   return (
     <main>
       <h2>Conteúdo Básico</h2>
@@ -15,17 +27,25 @@ export default function Conteudo() {
           <img src="https://placehold.co/600x400/1c1c1c1/00000/png" alt="Exemplo de Imagem" />
           <figcaption>Bloco de Imagem 600 x 400</figcaption>
         </figure>
-         {/*Esta imagem tem referencia externa*/}
+
+         {/*Esta imagem tem referencia interna na /src*/}
         <figure>
           <img src={imgQuadrado} alt="Exemplo de Imagem" />
           <figcaption>Bloco de Imagem 600 x 400</figcaption>
         </figure>
+
          {/*Esta imagem tem referencia interna na public*/}
         <figure>
           <img src="/png.png" alt="Exemplo de Imagem" />
           <figcaption>Bloco de Imagem 600 x 400</figcaption>
         </figure>
       </section>
+
+      <section>
+        <h2>Especial</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo deleniti dignissimos, praesentium et voluptas in temporibus assumenda eligendi delectus sapiente commodi molestias cum adipisci itaque nihil magni dolorum quo esse quos? Voluptas, eligendi. Placeat consectetur autem in delectus accusantium, modi rerum. Harum velit earum ea quaerat, corrupti maxime ipsum minus.</p>
+      </section>
+      <button onClick={alternaSecao}>{mostraSecao ? "ESCONDE" : "MOSTRAR"}</button>
     </main>
   );
 }
