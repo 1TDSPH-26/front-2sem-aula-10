68844db88caa0f75e4083329e1677a7d89531432
carlostsucamoto <caduchiarelli@hotmail.com>
2026-09-02 09:19:41 -0300

    feat: Exemplo de apresentação de imagem.

--- my-app/src/components/conteudo/Conteudo.tsx
+++ my-app/src/components/conteudo/Conteudo.tsx
@@ -2,8 +2,13 @@ export default function Conteudo() {
   return (
     <main>
         <section>
-            <h2>Conteúdo Básico</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos soluta sed vero reiciendis enim nam, aut perferendis, sit velit ratione placeat, distinctio eius rerum maxime id perspiciatis repudiandae non! Tenetur.</p>
+            <h2>CExemplo de imagem</h2>
+            <figure>
+                <img src= "https://placehold.co/600x400/1c1c1c1/FFFFFF/png" alt = "Exemplo de imagem"/>
+                <figcaption > Bloco de imagem 600 x 400 </figcaption>
+            </figure>
+            
+            
         </section>
     </main>
   )
