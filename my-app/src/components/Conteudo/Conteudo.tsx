83673f0f6b010d62b3da5cc4a07b5e9c40fe94d4
felipelima2005 <felipelimaoliveira2008@gmail.com>
2026-09-02 09:55:36 -0300

    feat: criação de uma section especial com useState

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import imgQuadrada from "../../img/png.png";
 
 export default function Conteudo(){
+
+
+        const[mostraSecao, setMostraSecao] = useState(false);
+
+        const alternaSecao = () =>{
+
+        setMostraSecao(!mostraSecao);
+        console.log(mostraSecao);
+    }
+    
     return(
         <main>
             <section>
@@ -28,6 +39,13 @@ export default function Conteudo(){
                 </figure>
 
             </section>
+
+            <section>
+                <h2>Especial</h2>   
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis tempore distinctio enim debitis quod quaerat cupiditate, optio eaque obcaecati nihil excepturi quam consequuntur, magnam beatae delectus. Doloribus placeat, recusandae reiciendis deleniti sequi est dicta doloremque eum nihil earum vero sed libero qui nesciunt dolorem minus alias nostrum hic quisquam laboriosam!</p>
+
+            </section>
+            <button onClick={alternaSecao}>{mostraSecao ? "ESCONDER": "MOSTRAR"}</button>
         </main>
     )
 }
