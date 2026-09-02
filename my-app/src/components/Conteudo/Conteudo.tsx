0a57627c0e8c3aa629e13ff1c5bfcba168f35d5f
carolinakotz <carolkotz@gmail.com>
2026-09-02 09:55:02 -0300

    feat:  Iniciando o useState esconde e mostra seção.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import imgQuadrada from"../../img/quadrado.png"; 
 
 export default function Conteudo() {
+
+    const[mostraSecao, setMostraSecao] = useState(false);
+
+    const alternaSecao = () => {
+
+        setMostraSecao(!mostraSecao);
+        console.log(mostraSecao);
+    }
 
     return (
         <main>
@@ -8,7 +17,7 @@ export default function Conteudo() {
                 <h2>Conteúdo Básico</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos soluta sed vero reiciendis enim nam, aut perferendis, sit velit ratione placeat, distinctio eius rerum maxime id perspiciatis repudiandae non! Tenetur.</p>
             </section>
-            
+
             <section>
                 <h2>Exemplo de Imagens</h2>
 
@@ -26,10 +35,18 @@ export default function Conteudo() {
 
                 {/*Esta imagem tem referência interna na public */}
                 <figure>
-                    <img src="/quadrado.png" alt="Exemplo de Imagem" />
+                    <img src="/img/quadrado.png" alt="Exemplo de Imagem" />
                     <figcaption>Bloco de Imagem 400x400</figcaption>
                 </figure>
             </section>
+
+            <section>
+                <h2>Especial</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illo dolore animi quod dolor tenetur quis aliquam ullam illum, odit perferendis officia nobis commodi id? Enim magnam error autem veritatis consequuntur, exercitationem assumenda rerum quas necessitatibus est reprehenderit ab praesentium quasi libero, id possimus numquam rem voluptate doloremque debitis consequatur?</p>
+            </section>
+            <button onClick={alternaSecao}>
+                {mostraSecao ? "Esconder Seção" : "Mostrar Seção"}
+            </button>
         </main>
     );
 }
