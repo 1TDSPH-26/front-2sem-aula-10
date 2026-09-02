69aec468a099ae3b355a533cfafd176db21b7c24
Nicolas Sousa <niolaspaixao34@gmail.com>
2026-09-02 09:19:05 -0300

    feat: Exemplo de apresentação de imagem.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -3,6 +3,14 @@ export default function Conteudo() {
     <main>
       <h2>Conteúdo Básico</h2>
       <p>Este é o conteúdo principal da minha aplicação.</p>
+
+      <section>
+        <h2>Exemplo de Imagem</h2>
+        <figure>
+          <img src="https://placehold.co/600x400/1c1c1c1/00000/png" alt="Exemplo de Imagem" />
+          <figcaption>Bloco de Imagem 600 x 400</figcaption>
+        </figure>
+      </section>
     </main>
   );
 }
