930790f3a94933640fa6258e022cf5af5f79dc58
carolinakotz <carolkotz@gmail.com>
2026-09-02 09:20:50 -0300

    feat: Exemplo de imagem.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,10 +1,17 @@
 export default function Conteudo() {
-    
+
     return (
         <main>
             <section>
                 <h2>Conteúdo Básico</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos soluta sed vero reiciendis enim nam, aut perferendis, sit velit ratione placeat, distinctio eius rerum maxime id perspiciatis repudiandae non! Tenetur.</p>
+            </section>
+            <section>
+                <h2>Exemplo de Imagens</h2>
+                <figure>
+                    <img src="https://placehold.co/400x400/1c1c1c1/000000/png" alt="Exemplo de Imagem" />
+                    <figcaption>Bloco de Imagem 400x400</figcaption>
+                </figure>
             </section>
         </main>
     );
