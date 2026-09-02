092aed64443744113392d6ccfb431bd2ce0ad45f
Nicolas Sousa <niolaspaixao34@gmail.com>
2026-09-02 09:30:20 -0300

    feat: Referências de imagens

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,3 +1,7 @@
+import imgQuadrado from "../../img/png.png";
+
+
+
 export default function Conteudo() {
   return (
     <main>
@@ -6,8 +10,19 @@ export default function Conteudo() {
 
       <section>
         <h2>Exemplo de Imagem</h2>
+        {/*Esta imagem tem referencia externa*/}
         <figure>
           <img src="https://placehold.co/600x400/1c1c1c1/00000/png" alt="Exemplo de Imagem" />
+          <figcaption>Bloco de Imagem 600 x 400</figcaption>
+        </figure>
+         {/*Esta imagem tem referencia externa*/}
+        <figure>
+          <img src={imgQuadrado} alt="Exemplo de Imagem" />
+          <figcaption>Bloco de Imagem 600 x 400</figcaption>
+        </figure>
+         {/*Esta imagem tem referencia interna na public*/}
+        <figure>
+          <img src="/png.png" alt="Exemplo de Imagem" />
           <figcaption>Bloco de Imagem 600 x 400</figcaption>
         </figure>
       </section>
